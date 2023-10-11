553c03a0286ef2d65927cef6faae847b6e452a3d
Sreys54 <s.reyesc2@uniandes.edu.co>
2023-10-10 20:42:14 -0500

    carga

---
+++ src/Logica/Categoria.java
@@ -0,0 +1,18 @@
+package Logica;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Aca van a ir todas las funciones relacionadas con la carga de datos de cada uno de los 
+ * archivos txt.
+ */
+
+public class Categoria 
+{
+	
+}
