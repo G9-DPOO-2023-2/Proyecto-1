20047d98776715470d898896c2bf83c9fd6dd30e
Sreys54 <s.reyesc2@uniandes.edu.co>
2023-10-10 23:12:33 -0500

    tenemos carga

--- src/Logica/Categoria.java
+++ src/Logica/Categoria.java
@@ -14,5 +14,133 @@ import java.util.List;
 
 public class Categoria 
 {
+	//*******************************************************************************
+	//Atributos
+	//*******************************************************************************
+	
+	private HashMap<String, List<String>> SUV;
+	private HashMap<String, List<String>> pequeno;
+	private HashMap<String, List<String>> lujo;
+	private HashMap<String, List<String>> vans;
+	private HashMap<String, List<String>> deportivo;
+	
+	
+	//**********************************************************************************
+	//Constructor
+	//**********************************************************************************
+	
+	public Categoria ()
+	{
+		SUV = new HashMap<>();
+		pequeno = new HashMap<>();
+		lujo = new HashMap<>();
+		vans = new HashMap<>();
+		deportivo = new HashMap<>();
+	}
+
+	//Metodos OJO recomendable poner setters and getters
+	
+	public HashMap<String, List<String>> getSUV()
+	{
+		return SUV;
+	}
+
+	public HashMap<String, List<String>> getPequeno() 
+	{
+		return pequeno;
+	}
+
+	public HashMap<String, List<String>> getLujo() 
+	{
+		return lujo;
+	}
+
+	public HashMap<String, List<String>> getVans() 
+	{
+		return vans;
+	}
+
+	public HashMap<String, List<String>> getDeportivo() 
+	{
+		return deportivo;
+	}
+	
+	//Metodos de Logica
+	
+	/**
+	 * Cada una de estas funciones carga los txt de los vehiculos y los almacena en cada 
+	 * uno de los HashMap respectivamente (dependiendo de su categoria)
+	 * @param archivo
+	 */
+	public void cargarVehiculos (String archivo)
+	{
+		try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(";");
+                // Usar la primera parte como clave
+                String placa = partes[0];
+                String categoria = partes[9];
+                
+                // Agregar las demás partes a una lista
+                List<String> valores = new ArrayList<>();          
+                if (categoria.equals("suv"))
+                {
+	                for (int i = 1; i < partes.length; i++) {
+	                    valores.add(partes[i]);
+	                }
+	                SUV.put(placa, valores);
+                }
+
+                // Insertar la clave y la lista de valores en el HashMap
+                if (categoria.equals("pequeño"))
+                {
+                	 for (int i = 1; i < partes.length; i++) {
+ 	                    valores.add(partes[i]);
+ 	                }
+                	 pequeno.put(placa, valores); 
+                }
+                
+                if (categoria.equals("lujo"))
+                {
+                	 for (int i = 1; i < partes.length; i++) {
+ 	                    valores.add(partes[i]);
+ 	                }
+                	 lujo.put(placa, valores); 
+                }
+                
+                if (categoria.equals("vans"))
+                {
+                	 for (int i = 1; i < partes.length; i++) {
+ 	                    valores.add(partes[i]);
+ 	                }
+                	 vans.put(placa, valores); 
+                }
+                
+                if (categoria.equals(" Deportivo"))
+                {
+                	 for (int i = 1; i < partes.length; i++) {
+ 	                    valores.add(partes[i]);
+ 	                }
+                	 deportivo.put(placa, valores); 
+                }
+            }
+            br.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Imprimir el HashMap
+        System.out.println(SUV);
+        System.out.println(pequeno);
+        System.out.println(lujo);
+        System.out.println(deportivo);
+        System.out.println(vans);
+        
+
+    }
 	
 }
+	
+	
+
--- src/Presentacion/Aplicacion.java
+++ src/Presentacion/Aplicacion.java
@@ -1,5 +1,24 @@
 package Presentacion;
 
-public class Aplicacion {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+import Logica.AdminGeneral;
+import Logica.AdminLocal;
+import Logica.Categoria;
+import Logica.ClienteRegistrado;
+import Logica.Empleado;
+import Logica.Sede;
+import Logica.Vehiculo;
+
+public class Aplicacion 
+{
+	public static void main(String[] args) 
+	{
+		Categoria x = new Categoria();
+		x.cargarVehiculos("vehiculos.txt");
+		System.out.println(x);
+	}
 }
