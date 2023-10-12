aaa1f2cc3b0ed52eef7c5eb961daccfdcf23cf5c
Sreys54 <s.reyesc2@uniandes.edu.co>
2023-10-11 20:11:36 -0500

    si

--- src/Presentacion/Aplicacion.java
+++ src/Presentacion/Aplicacion.java
@@ -16,17 +16,92 @@ import Logica.BaseDatos;
 
 public class Aplicacion 
 {
-	public static void main(String[] args) 
+	private Categoria categoria = new Categoria ();
+	private BaseDatos baseDatos = new BaseDatos();
+	
+	/**
+	 * Ejecuta la aplicación: le muestra el menú al usuario y la pide que ingrese
+	 * una opción, y ejecuta la opción seleccionada por el usuario. Este proceso se
+	 * repite hasta que el usuario seleccione la opción de abandonar la aplicación.
+	 */
+	public void ejecutarAplicacion()
 	{
-		Categoria x = new Categoria();
-		x.cargarVehiculos("vehiculos.txt");
-		System.out.println(x);
+		System.out.println("\nOpciones sobre el menu");
 		
-		BaseDatos y = new BaseDatos();
-		y.cargarEmpleados("empleados.txt");
-		System.out.println(y);
+		boolean continuar = true;
+		while (continuar)
+		{
+			try
+			{
+				mostrarMenu();
+				int opcion_seleccionada = Integer.parseInt(input("Por favor seleccione una opción"));
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println("Debe seleccionar uno de los números de las opciones.");
+			}
+		}
+	}
+	
+	/**
+	 * Muestra al usuario el menú con las opciones para que escoja la siguiente
+	 * acción que quiere ejecutar.
+	 * 
+	 */
+	public void mostrarMenu()
+	{
+		System.out.println("\nOpciones de la aplicación\n");
+		System.out.println("1. Iniciar Sesión");
+		System.out.println("2. Crear Cuenta");
+	}
+	
+	public void ejecutarInicioSesion()
+	{
+		String usuario = input("Por favor ingrese su usuario: ");
+		String contraseña = input ("Por favor ingrese su contraseña: ");
 		
-		y.cargarClientesRegistrados("ClientesRegistrados.txt");
-		System.out.println(y);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Este método sirve para imprimir un mensaje en la consola pidiéndole
+	 * información al usuario y luego leer lo que escriba el usuario.
+	 * 
+	 * @param mensaje El mensaje que se le mostrará al usuario
+	 * @return La cadena de caracteres que el usuario escriba como respuesta.
+	 */
+	public String input(String mensaje)
+	{
+		try
+		{
+			System.out.print(mensaje + ": ");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			return reader.readLine();
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error leyendo de la consola");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static void main(String[] args)
+	{
+		Aplicacion consola = new Aplicacion();
+		consola.ejecutarAplicacion();
 	}
 }
